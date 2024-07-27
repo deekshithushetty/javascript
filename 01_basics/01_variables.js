c379abbb7b1516e59a405169e6b97498f483d6f9
deekshithushetty <deekshithushetty21@gmail.com>
2024-07-27 05:36:35 +0000

    learner variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 13345
+let accountEmail = "deekshith@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mangaluru"
+
+// accountId = 2 
+
+accountEmail = "deek@gmail.com"
+
+accountPassword = "9876"
+
+accountCity = "Ullal"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
